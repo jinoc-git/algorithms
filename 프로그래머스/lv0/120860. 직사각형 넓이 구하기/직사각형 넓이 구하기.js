13d156779706e01eb47ed25e65c89b4e242a136c
jinoc <132889294+jinoc-git@users.noreply.github.com>
2023-08-03 10:15:08 +0900

    [level 0] Title: 직사각형 넓이 구하기, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120860. 직사각형 넓이 구하기/직사각형 넓이 구하기.js
+++ 프로그래머스/lv0/120860. 직사각형 넓이 구하기/직사각형 넓이 구하기.js
@@ -1,10 +1,10 @@
 function solution(dots) {
     let answer = 0;
-    const x = dots.map((dot) => dot[0]).sort();
-    const y = dots.map((dot) => dot[1]).sort();
-    
-    const row = Math.abs(x[3] - x[0]);
-    const column = Math.abs(y[3] - y[0]);
+    const x = dots.map((dot) => dot[0]).sort((a, b) => a - b < 0 ? -1 : 1);
+    const y = dots.map((dot) => dot[1]).sort((a, b) => a - b < 0 ? -1 : 1);
+
+    const row = x[3] - x[0];
+    const column = y[3] - y[0];
     answer = row * column;
     
     return answer;
