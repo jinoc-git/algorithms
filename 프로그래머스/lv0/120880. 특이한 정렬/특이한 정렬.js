82692a059f7787c60d258e048a4de9d6953bda02
jinoc <132889294+jinoc-git@users.noreply.github.com>
2023-08-18 10:27:09 +0900

    [level 0] Title: 특이한 정렬, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120880. 특이한 정렬/특이한 정렬.js
+++ 프로그래머스/lv0/120880. 특이한 정렬/특이한 정렬.js
@@ -11,41 +11,7 @@ function solution(numlist, n) {
         }
     });
 
-
-
     return answer;
 }
 
 
-//     let small = [];
-//     let big = [];
-//     console.log(sortArr)
-//     for (let i = 0; i < sortArr.length; i++) {
-//         if (sortArr[i] <= n) {
-//             small = [...sortArr].splice(0, i + 1).reverse();
-//             big = [...sortArr].splice(i + 1) 
-//         } else {
-//             break;
-//         }
-//     }
-//     console.log(small, big)
-    
-//     for (let i = 0; i < sortArr.length; i++) {
-//         if (!small[i]) {
-//             const etc = big.splice(i);
-//             answer = [...answer, ...etc];
-//             break;
-//         }
-//         if (!big[i]) {
-//             const etc = small.splice(i);
-//             answer = [...answer, ...etc];
-//             break;
-//         }
-//         if (n - small[i] < big[i] - n) {
-//             answer.push(small[i]);
-//             answer.push(big[i]);
-//         } else {
-//             answer.push(big[i]);
-//             answer.push(small[i]);
-//         }
-//     }
