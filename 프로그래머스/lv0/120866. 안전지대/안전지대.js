465a3c144a795643a43621ff0decf837d16451c9
jinoc <132889294+jinoc-git@users.noreply.github.com>
2023-10-02 21:26:00 +0900

    [level 0] Title: 안전지대, Time: 0.42 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120866. 안전지대/안전지대.js
@@ -0,0 +1,60 @@
+function solution(board) {
+    let answer = 0;
+    const dangerZone = new Map();
+    
+    for (let i = 0; i < board.length; i++) {
+        const row = board[i];
+        const boomIndex = [];
+        
+        for (let j = 0; j < row.length; j++) {
+            const space = row[j];
+            
+            if (space === 1) boomIndex.push(j);
+            else boomIndex.push(true);
+        }
+
+        dangerZone.set(i, boomIndex);
+    }
+    
+    dangerZone.forEach((rowValue, rowNum, zone) => {
+        for (let space of rowValue) {
+            const isDanger = typeof space === 'number';
+            if (isDanger) {
+                const beforeRow = zone.get(rowNum - 1);
+                if (beforeRow) {
+                    const newBeforeRow = beforeRow.map((item, idx) => {
+                        const needChange = idx === space - 1 || idx === space || idx === space + 1;
+                        if (needChange) return false;
+                        return item;
+                    })
+                    zone.set(rowNum - 1, newBeforeRow);
+                }
+                
+                const afterRow = zone.get(rowNum + 1);
+                if (afterRow) {
+                    const newAfterRow = afterRow.map((item, idx) => {
+                        const needChange = idx === space - 1 || idx === space || idx === space + 1;
+                        if (needChange && typeof item !== 'number') return false;
+                        return item;
+                    })
+                    zone.set(rowNum + 1, newAfterRow);
+                }
+                
+                const currentRow = zone.get(rowNum);
+                const newCurrentRow = currentRow.map((item, idx) => {
+                    const needChange = idx === space - 1 || idx === space || idx === space + 1;
+                    if (needChange && typeof item !== 'number') return false;
+                    return item;
+                })
+                zone.set(rowNum, newCurrentRow);
+            } 
+        }
+    })
+    
+    dangerZone.forEach((rowValue) => {
+        const safeZone = rowValue.filter((space) => space === true);
+        answer += safeZone.length;
+    })
+    
+    return answer;
+}
