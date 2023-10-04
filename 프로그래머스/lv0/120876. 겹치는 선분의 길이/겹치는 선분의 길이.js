6f19375622b8cb43f11cb36d8a8fe05c4ece07f3
jinoc <132889294+jinoc-git@users.noreply.github.com>
2023-10-04 21:52:53 +0900

    [level 0] Title: 겹치는 선분의 길이, Time: 0.20 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120876. 겹치는 선분의 길이/겹치는 선분의 길이.js
@@ -0,0 +1,32 @@
+function getLength(line) {
+    const start = line[0];
+    const end = line[1];
+    const length = end - start;
+    return length;
+}
+
+function solution(lines) {
+    let answer = 0;
+    const [first, second, third] = lines;
+    
+    const minDot = Math.min(first[0], second[0], third[0]);
+    const maxDot = Math.max(first[1], second[1], third[1]);
+    
+    const baseLineMap = new Map();
+    for (let dot = minDot; dot < maxDot; dot++) baseLineMap.set(dot, 0);
+
+    lines.forEach(([start, end]) => {
+        for (let i = start; i < end; i++) {
+            const dot = baseLineMap.get(i)
+            baseLineMap.set(i, dot + 1);
+        }
+    })
+
+    baseLineMap.forEach((dot) => {
+        if (dot > 1) answer++
+    })
+    
+    
+    return answer;
+}
+
