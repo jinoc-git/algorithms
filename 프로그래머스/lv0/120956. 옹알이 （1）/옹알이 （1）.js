f6535c2e2a6ff9848a200e694f84240d8ca436ac
jinoc <132889294+jinoc-git@users.noreply.github.com>
2023-10-06 21:37:53 +0900

    [level 0] Title: 옹알이 (1), Time: 0.17 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120956. 옹알이 （1）/옹알이 （1）.js
+++ 프로그래머스/lv0/120956. 옹알이 （1）/옹알이 （1）.js
@@ -3,24 +3,13 @@ function solution(babbling) {
     const possibleLetterList = new Map([['a', 'aya'], ['y', 'ye'], ['w', 'woo'], ['m', 'ma']]);
 
     for (let word of babbling) {
-        let copyWord = word;
-        let firstLetter = word[0];
+        let copyWord = word;        
         
-        const isPossibleLetter = possibleLetterList.has(firstLetter);
-        if (!isPossibleLetter) continue;
-        
-        const checkedWord = [];
-        
-        for (let n = 0; checkedWord.length < 4; n++) {
+        for (let n = 0; n < 4; n++) {
             const checkingWord = possibleLetterList.get(copyWord[0]);
             if (!checkingWord) break;
             
-            const isChecked = checkedWord.find((v) => v === checkingWord);
-            if (isChecked) break;
-
-            checkedWord.push(checkingWord);
-            const wordLength = checkingWord.length;
-            
+            const wordLength = checkingWord.length;        
             const isSameWord = copyWord.slice(0, wordLength) === checkingWord;
             if (!isSameWord) break;
             
