035d66ea6643ce3efa655df2f4b2cd7246e436ca
jinoc <132889294+jinoc-git@users.noreply.github.com>
2023-10-10 19:54:01 +0900

    [level 1] Title: 문자열 내림차순으로 배치하기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12917. 문자열 내림차순으로 배치하기/문자열 내림차순으로 배치하기.js
+++ 프로그래머스/1/12917. 문자열 내림차순으로 배치하기/문자열 내림차순으로 배치하기.js
@@ -1,18 +1,5 @@
 function solution(s) {
-    let answer = '';
-    let lowerCase = '';
-    let upperCase = '';
-    
-    for (let str of s) {
-        const isUpperCase = str.toUpperCase() === str;
-        if (isUpperCase) upperCase += str;
-        else lowerCase += str;
-    }
-    
-    const sortedLowerCase = lowerCase.split('').sort((a, b) => b.localeCompare(a)).join('');
-    const sortedUpperCase = upperCase.split('').sort((a, b) => b.localeCompare(a)).join('');
-    
-    answer = sortedLowerCase + sortedUpperCase;
+    const answer = s.split('').sort().reverse().join('')
     
     return answer;
 }
