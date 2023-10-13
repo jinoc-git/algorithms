957cf355194e8c5b6e88b698a3a98828f4d703d3
jinoc <132889294+jinoc-git@users.noreply.github.com>
2023-10-13 21:26:53 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 0.13 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.js
+++ 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.js
@@ -1,30 +1,24 @@
 function solution(n, m) {
-    const answer = [];
     let GCD = 1;
     let LCM = 1;
-    const limit = n > m ? n / 2 : m / 2;
     let num = 2;
     
-    const shouldDivide = num <= limit;
-    while (shouldDivide) {
-        if (num > n || num > m) break;
-        
+    while (num <= n || num <= m) {
         const isAliqOfN = n % num === 0;
         const isAliqOfM = m % num === 0;
         
         if (isAliqOfN && isAliqOfM) {
             GCD *= num;
             LCM *= num;
-            n = n / num;
-            m = m / num;
+            n /= num;
+            m /= num;
         }
         else num++;
     }
     
     LCM *= n * m;
     
-    answer.push(GCD);
-    answer.push(LCM);
+    const answer = [GCD, LCM];
     
     return answer;
 }
