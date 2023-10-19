8c8bd7b62f6e885c11b39f4b68373d9bc1ecbbc6
jinoc <132889294+jinoc-git@users.noreply.github.com>
2023-10-19 18:22:48 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
@@ -0,0 +1,29 @@
+const NUMBER = {
+    'zero': '0',
+    'one': '1',
+    'two': '2',
+    'three': '3',
+    'four': '4',
+    'five': '5',
+    'six': '6',
+    'seven': '7',
+    'eight': '8',
+    'nine': '9',
+};
+
+function solution(s) {
+    let answer = '';
+    let word = '';
+    
+    for (let str of s) {
+        if (isNaN(str)) word += str;
+        else answer += str;
+        
+        if (NUMBER[word]) {
+            answer += NUMBER[word];
+            word = '';
+        }
+    }
+
+    return Number(answer);
+}
