d4c4165bb1f9462dbfa44a99e1af8c875db4d9b5
jinoc <132889294+jinoc-git@users.noreply.github.com>
2023-10-19 18:03:05 +0900

    [level 1] Title: 최소직사각형, Time: 2.84 ms, Memory: 38.9 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.js
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.js
@@ -1,22 +1,22 @@
 function solution(sizes) {
     let answer = 0;
-    let longSide = [];
-    let smallSide = [];
+    let longestWidth = 0;
+    let longestHeight = 0;
     
     for (let card of sizes) {
         const cardWidth = card[0];
         const cardHeight = card[1];
         if (cardWidth > cardHeight) {
-            longSide.push(cardWidth);
-            smallSide.push(cardHeight);
+            longestWidth = longestWidth > cardWidth ? longestWidth : cardWidth;
+            longestHeight = longestHeight > cardHeight ? longestHeight : cardHeight;
         }
-        else {
-            longSide.push(cardHeight);
-            smallSide.push(cardWidth); 
+        if (cardWidth <= cardHeight) {
+            longestWidth = longestWidth > cardHeight ? longestWidth : cardHeight;
+            longestHeight = longestHeight > cardWidth ? longestHeight : cardWidth;
         }
     }
     
-    answer = Math.max(...longSide) * Math.max(...smallSide);
+    answer = longestWidth * longestHeight;
     
     return answer;
 }
