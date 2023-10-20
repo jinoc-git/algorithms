4e260cf94dbf982fcf8c861fc49adc77dc4f83af
jinoc <132889294+jinoc-git@users.noreply.github.com>
2023-10-20 18:31:41 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.17 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
@@ -0,0 +1,30 @@
+function solution(n, arr1, arr2) {
+    const answer = [];
+    
+    const changeFormat = (num) => {
+        let result = '';
+        for (let i = 0; i < n; i++) {
+            const rest = num % 2;
+            result = String(rest) + result;
+            num = Math.floor(num / 2);
+        }
+        
+        return result;
+    }
+    
+    for (let i = 0; i < n; i++) {
+        let row = '';
+        const num1 = changeFormat(arr1[i]);
+        const num2 = changeFormat(arr2[i]);
+        
+        for (let j = 0; j < n; j++) {
+            const isWall = num1[j] === '1' || num2[j] === '1';
+            if (isWall) row += '#';
+            else row += ' ';
+        }
+        
+        answer.push(row);
+    }
+    
+    return answer;
+}
