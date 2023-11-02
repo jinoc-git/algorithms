e17a42a1ffffa64bb61e5897acd48ec656f69a14
jinoc <132889294+jinoc-git@users.noreply.github.com>
2023-11-02 16:28:26 +0900

    [level 1] Title: 추억 점수, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/176963. 추억 점수/추억 점수.js
+++ 프로그래머스/1/176963. 추억 점수/추억 점수.js
@@ -1,14 +1,11 @@
 function solution(name, yearning, photo) {
     const answer = [];
-    const score = new Map();
-    
-    for (let i = 0; i < name.length; i++) score.set(name[i], yearning[i]);
     
     photo.forEach((nameList) => {
         let sum = 0;
         for (let val of nameList) {
-            const nameScore = score.get(val);
-            if (nameScore !== undefined) sum += nameScore;
+            const idx = name.indexOf(val);
+            if (idx !== -1) sum += yearning[idx];
         }
         
         answer.push(sum);
@@ -16,3 +13,22 @@ function solution(name, yearning, photo) {
     
     return answer;
 }
+
+// function solution(name, yearning, photo) {
+//     const answer = [];
+//     const score = new Map();
+    
+//     for (let i = 0; i < name.length; i++) score.set(name[i], yearning[i]);
+    
+//     photo.forEach((nameList) => {
+//         let sum = 0;
+//         for (let val of nameList) {
+//             const nameScore = score.get(val);
+//             if (nameScore !== undefined) sum += nameScore;
+//         }
+        
+//         answer.push(sum);
+//     })
+    
+//     return answer;
+// }
