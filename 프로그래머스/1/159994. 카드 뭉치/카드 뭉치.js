774b6a974f180ceb51bca98935abcb5b1aa29e16
jinoc <132889294+jinoc-git@users.noreply.github.com>
2023-11-11 22:02:52 +0900

    [level 1] Title: 카드 뭉치, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.js
@@ -0,0 +1,17 @@
+function solution(cards1, cards2, goal) {
+    let answer = 'Yes';
+    
+    for (let i = 0; i < goal.length; i++) {
+        const keyword = goal[i];
+        const card1 = cards1[0];
+        const card2 = cards2[0];
+        
+        if (keyword === card1) cards1.shift();
+        if (keyword === card2) cards2.shift();
+        
+        const noMatchWord = keyword !== card1 && keyword !== card2;
+        if (noMatchWord) answer = 'No';
+    }
+    
+    return answer;
+}
