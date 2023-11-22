cc19df1fc6c66b44dee081effda54c569116d222
jinoc <132889294+jinoc-git@users.noreply.github.com>
2023-11-22 23:31:03 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 15.54 ms, Memory: 42.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.js
@@ -0,0 +1,24 @@
+function solution(queue1, queue2) {
+    const queue = queue1.concat(queue2);
+    let sumQ1 = queue1.reduce((acc, cur) => acc + cur, 0);
+    let sumQ2 = queue2.reduce((acc, cur) => acc + cur, 0);    
+    const half = (sumQ1 + sumQ2) / 2;
+    
+    let q1Pointer = 0;
+    let q2Pointer = queue1.length;
+    
+    for (let i = 0; i < queue.length * 3; i++) {
+        if (sumQ1 === half) return i;
+
+        if (sumQ1 > half) {
+            sumQ1 -= queue[q1Pointer % queue.length];
+            q1Pointer++;
+        } 
+        else if (sumQ1 < half) {
+            sumQ1 += queue[q2Pointer % queue.length];
+            q2Pointer++;
+        }
+    }
+    
+    return -1;
+}
