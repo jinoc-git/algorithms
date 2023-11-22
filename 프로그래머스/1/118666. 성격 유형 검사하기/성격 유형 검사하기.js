8dbb20a05f97c29a17a1e44bbe242244752aa4ec
jinoc <132889294+jinoc-git@users.noreply.github.com>
2023-11-22 19:53:43 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 0.61 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.js
+++ 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.js
@@ -1,15 +1,6 @@
 function solution(survey, choices) {
     let answer = '';
-    const character = {
-        R: 0,
-        T: 0,
-        C: 0,
-        F: 0,
-        J: 0,
-        M: 0,
-        A: 0,
-        N: 0,
-    }
+    const character = { R: 0, T: 0, C: 0, F: 0, J: 0, M: 0, A: 0, N: 0 }
     
     for (let i = 0; i < survey.length; i++) {
         const [type1, type2] = survey[i].split('');
@@ -20,16 +11,16 @@ function solution(survey, choices) {
     }
     
     if (character['R'] < character['T']) answer += 'T';
-    if (character['R'] >= character['T']) answer += 'R';
+    else answer += 'R';
     
     if (character['C'] < character['F']) answer += 'F';
-    if (character['C'] >= character['F']) answer += 'C';
+    else answer += 'C';
     
     if (character['J'] < character['M']) answer += 'M';
-    if (character['J'] >= character['M']) answer += 'J';
+    else answer += 'J';
     
     if (character['A'] < character['N']) answer += 'N';
-    if (character['A'] >= character['N']) answer += 'A';
+    else answer += 'A';
     
     return answer;
 }
