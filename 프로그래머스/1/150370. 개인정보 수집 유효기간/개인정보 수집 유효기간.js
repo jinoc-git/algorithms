444ac6d02b8726b0592240884a7d06dd258cae48
jinoc <132889294+jinoc-git@users.noreply.github.com>
2023-11-24 18:47:51 +0900

    [level 1] Title: 개인정보 수집 유효기간, Time: 0.56 ms, Memory: 33.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.js
@@ -0,0 +1,37 @@
+function solution(today, terms, privacies) {
+    const answer = [];
+    const [tYear, tMonth, tDay] = today.split('.');
+    const termsMap = new Map();
+    
+    terms.forEach((item) => {
+        const [key, val] = item.split(' ');
+        termsMap.set(key, val);
+    })
+
+    privacies.forEach((privacy, idx) => {
+        const [date, privacyTerms] = privacy.split(' ');
+        const [year, month, day] = date.split('.');
+        const maxMonth = termsMap.get(privacyTerms);
+        
+        
+        let delYear = +year + Math.floor(maxMonth / 12);
+        let delMonth = +month + (+maxMonth % 12);
+
+        if (delMonth > 12) {
+            delYear += 1;
+            delMonth %= 12;
+        }
+
+        if (delYear > +tYear) return;
+        
+        const isSameYear = delYear === +tYear;
+        if (isSameYear && delMonth > +tMonth) return;
+
+        const isSameMonth = delMonth === +tMonth;
+        if (isSameYear && isSameMonth && +day > +tDay) return;
+        
+        answer.push(idx + 1);
+    })
+    
+    return answer;
+}
