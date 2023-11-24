035292795c5160e968573b51de9ad849ce22a079
jinoc <132889294+jinoc-git@users.noreply.github.com>
2023-11-24 21:03:15 +0900

    [level 1] Title: 소수 만들기, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12977. 소수 만들기/소수 만들기.js
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.js
@@ -1,21 +1,26 @@
+const isPrime = (num) => {
+    let n = 2;
+    while (n <= Math.sqrt(num)) {
+        if (num % n === 0) return false;
+        n++;
+    }
+        
+    return true;
+}
+
 function solution(nums) {
     let answer = 0;
-    
+
     for (let i = 0; i < nums.length - 2; i++) {
         for (let j = i + 1; j < nums.length - 1; j++) {
             for (let k = j + 1; k < nums.length; k++) {
                 const sum = nums[i] + nums[j] + nums[k]; 
  
-                let n = 2;
-                while (n < sum) {
-                    if (sum % n === 0) break;
-                    n++;
-                }
-
-                if (n === sum) answer++;
+                if (isPrime(sum)) answer++;
             }
         }
     }
     
     return answer;
 }
+
