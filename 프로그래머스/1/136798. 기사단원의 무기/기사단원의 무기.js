08cccb627587f2dad1a5900d8e5a0b92aa510cb8
jinoc <132889294+jinoc-git@users.noreply.github.com>
2023-11-28 18:53:41 +0900

    [level 1] Title: 기사단원의 무기, Time: 2.83 ms, Memory: 36.5 MB -BaekjoonHub

--- 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.js
+++ 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.js
@@ -2,7 +2,7 @@ function solution(number, limit, power) {
     let answer = 0;
     
     for (let i = 1; i <= number; i++) {
-        let count = 1 ;
+        let count = 1;
         
         for (let n = 1; n <= i / 2; n++) {
             if (i % n === 0) count++;
