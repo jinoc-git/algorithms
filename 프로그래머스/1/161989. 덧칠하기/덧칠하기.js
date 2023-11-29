fdbd43f9b6d570a0387b222d2ff4b48642956dfa
jinoc <132889294+jinoc-git@users.noreply.github.com>
2023-11-29 21:00:56 +0900

    [level 1] Title: 덧칠하기, Time: 1.40 ms, Memory: 37.4 MB -BaekjoonHub

--- 프로그래머스/1/161989. 덧칠하기/덧칠하기.js
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.js
@@ -1,14 +1,14 @@
 function solution(n, m, section) {
     if (m === 1) return section.length;
     
-    let answer = 1;
-    let rePaint = section[0] + m - 1;
+    let answer = 0;
+    let rePaint = 0;
     
     section.forEach((wallNum) => {
-        if (wallNum <= rePaint) return;
-        
-        rePaint = wallNum + m - 1;
-        answer++;
+        if (rePaint < wallNum) {
+            rePaint = wallNum + m - 1;
+            answer++;
+        }
     })
     
     return answer;
