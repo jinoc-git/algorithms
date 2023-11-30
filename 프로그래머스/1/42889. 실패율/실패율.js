fb975d98e3e894d65c6dcf273e4aaac072f9b460
jinoc <132889294+jinoc-git@users.noreply.github.com>
2023-12-01 00:34:24 +0900

    [level 1] Title: 실패율, Time: 0.13 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/실패율.js
+++ 프로그래머스/1/42889. 실패율/실패율.js
@@ -1,6 +1,6 @@
 function solution(N, stages) {
     let users = stages.length;
-    const stage = new Array(N + 1).fill([0, 0]);
+    const stage = new Array(N + 1).fill([null, 0]);
 
     for (let i = 0; i < stages.length; i++) {
         const userStage = stages[i];
@@ -11,12 +11,13 @@ function solution(N, stages) {
     const failureRate = stage.map(([currentStage, count], idx) => {
         const failure = count / users;
         users -= count;
-        return [currentStage === 0 ? idx + 1 : currentStage, failure];
+        return [currentStage ?? idx + 1 , failure];
     })
     
     failureRate.pop();
     
-    const answer = failureRate.sort((a, b) => b[1] - a[1]).map(([currentStage, failure]) => currentStage);
+    const answer = failureRate.sort(([_, aFailure], [__, bFailure]) => bFailure - aFailure)
+        .map(([currentStage, failure]) => currentStage);
     
     return answer;
 }
