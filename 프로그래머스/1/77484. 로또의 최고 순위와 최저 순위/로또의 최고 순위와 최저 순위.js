c7768bf126621bf34eb126a36827b77118e6ec8c
jinoc <132889294+jinoc-git@users.noreply.github.com>
2023-12-04 22:37:05 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.js
+++ 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.js
@@ -1,5 +1,5 @@
 function solution(lottos, win_nums) {
-    const rank = { 0: 6, 1: 6, 2: 5, 3: 4, 4: 3, 5: 2, 6: 1};
+    const rank = [6, 6, 5, 4, 3, 2, 1];
     const knownNum = lottos.filter((num) => num !== 0);
     let matchNum = 0;
     
