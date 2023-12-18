643a9827d872186b9dd48a57155c05601bef70d7
jinoc <132889294+jinoc-git@users.noreply.github.com>
2023-12-18 22:40:00 +0900

    [level 2] Title: 의상, Time: 0.18 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/42578. 의상/의상.js
@@ -0,0 +1,15 @@
+function solution(clothes) {
+    let answer = 1;
+    const clothesCount = new Map();
+    
+    for (let [name, type] of clothes) {
+        const typeCount = clothesCount.get(type) ?? 0;
+        clothesCount.set(type, typeCount + 1);
+    }
+    
+    const typeCountList = clothesCount.values();
+    
+    for (let count of typeCountList) answer *= count + 1;
+    
+    return answer - 1;
+}
