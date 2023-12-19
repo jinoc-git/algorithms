ee149f68b36a41a41dc2b7c60e83260076c9cf6f
jinoc <132889294+jinoc-git@users.noreply.github.com>
2023-12-19 21:12:38 +0900

    [level 2] Title: 가장 큰 수, Time: 0.06 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.js
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.js
@@ -1,8 +1,6 @@
 function solution(numbers) {
     const answer = numbers.map((n) => String(n)).sort((str1, str2) => {
-        if (str1 + str2 < str2 + str1) return 1;
-        if (str1 + str2 > str2 + str1) return -1;
-        if (str1 + str2 === str2 + str1) return 0;
+        return (str2 + str1) - (str1 + str2);
     }).join('');
 
     if (+answer === 0) return "0";
