4297ec96249b8c8ea5a042d443b20ccb3cbd703b
jinoc <132889294+jinoc-git@users.noreply.github.com>
2023-12-20 20:15:02 +0900

    [level 2] Title: H-Index, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/42747. H－Index/H－Index.js
@@ -0,0 +1,14 @@
+function solution(citations) {
+    let answer = 0;
+    const sortedCitations = citations.sort((a, b) => b - a);
+
+    if (sortedCitations[0] === 0) return answer;
+    
+    for (let i = 1; i <= sortedCitations.length; i++) {
+        const citationCount = sortedCitations[i - 1];
+        if (citationCount <= i) answer = Math.max(answer, citationCount);
+        else answer = Math.max(answer, i);
+    }
+    
+    return answer;
+}
