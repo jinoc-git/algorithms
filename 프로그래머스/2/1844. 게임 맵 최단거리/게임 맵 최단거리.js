0b5fd4bbf97ed67cd24c1878806a4b979a91e957
jinoc <132889294+jinoc-git@users.noreply.github.com>
2023-12-21 21:42:01 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 5.70 ms, Memory: 37.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
@@ -0,0 +1,27 @@
+function solution(maps) {
+    const xMax = maps[0].length;
+    const yMax = maps.length;
+    const goX = [1, -1, 0, 0];
+    const goY = [0, 0, 1, -1];
+    
+    const visited = [[0, 0, 1]];
+    maps[0][0] = 0;
+    
+    while (visited.length) {
+        const [y, x, count] = visited.shift();
+        
+        for (let i = 0; i < 4; i++) {
+            const nextX = x + goX[i];
+            const nextY = y + goY[i];
+            
+            if (nextX === xMax - 1 && nextY === yMax - 1) return count + 1;
+            
+            if (nextX >= 0 && nextX < xMax && nextY >= 0 && nextY < yMax && maps[nextY][nextX] === 1) {
+                maps[nextY][nextX] = 0;
+                visited.push([nextY, nextX, count + 1]);
+            }
+        }
+    }
+    
+    return -1;
+}
