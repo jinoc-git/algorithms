70736a7571fbd1170cec23d040920d117120ab4b
jinoc <132889294+jinoc-git@users.noreply.github.com>
2024-01-17 20:53:52 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 3.56 ms, Memory: 37.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.js
@@ -0,0 +1,38 @@
+function solution(n, wires) {
+    let answer = 100;
+    const nodes = {};
+    
+    wires.forEach(([v1, v2]) => {
+        if (!nodes[v1]) nodes[v1] = [];
+        if (!nodes[v2]) nodes[v2] = [];
+        
+        nodes[v1].push(v2);
+        nodes[v2].push(v1);
+    });
+    
+    const connection = (start, excluded) => {
+        let count = 0;
+        const way = [start];
+        const visited = [];
+        
+        while (way.length) {
+            const current = way.shift();
+            visited[current] = true;
+            count++;
+
+            nodes[current].forEach((point) => {
+                if (!visited[point] && point !== excluded) way.push(point);
+            })
+        }
+        
+        return count;
+    }
+    
+    wires.forEach(([v1, v2]) => {
+        const gap = Math.abs(connection(v1, v2) - connection(v2, v1));
+        answer = Math.min(answer, gap);
+    });
+    
+    
+    return answer;
+}
