5794846aed54c625f52f11c9a4e8bfd0a16f528f
jinoc <132889294+jinoc-git@users.noreply.github.com>
2024-02-23 20:16:12 +0900

    [level 1] Title: 신규 아이디 추천, Time: 0.19 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.js
+++ 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.js
@@ -1,23 +1,13 @@
 function solution(new_id) {
-    let answer = '';
-    
-    for (let str of new_id) answer += str.toLowerCase();
-    
-    const step2Regex = /[^a-z0-9\-_\.]/g;
-    answer = answer.replace(step2Regex, '');
+    let answer = new_id
+        .toLowerCase()
+        .replace(/[^\w-_.]/g, '')
+        .replace(/\.+/g, '.')
+        .replace(/^\.|\.$/g, '')
+        .padEnd(1, 'a')
+        .substring(0, 15).replace(/\.$/, '')
 
-    const step3Regex = /\.+/g;
-    answer = answer.replace(step3Regex, '.');
-    
-    if (answer[0] === '.') answer = answer.substring(1);
-    if (answer[answer.length - 1] === '.') answer = answer.substring(0, answer.length - 1);
-    
-    if (answer === '') answer = 'a';
-    
-    if (answer.length >= 16) answer = answer.substring(0, 15);
-    if (answer[answer.length - 1] === '.') answer = answer.substring(0, answer.length - 1);
-    
-    while (answer.length <= 2) answer += answer[answer.length - 1];
+    answer = answer.padEnd(3, answer[answer.length - 1]);
     
     return answer;
 }
