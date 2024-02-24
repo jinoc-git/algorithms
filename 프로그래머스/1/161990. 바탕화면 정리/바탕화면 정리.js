d76a58baeb5f844b9847bd13ae435361db1524ac
jinoc <132889294+jinoc-git@users.noreply.github.com>
2024-02-24 22:47:04 +0900

    [level 1] Title: 바탕화면 정리, Time: 0.25 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.js
@@ -0,0 +1,26 @@
+function solution(wallpaper) {
+    const answer = [51, 51, 0, 0];
+    
+    wallpaper.forEach((row, rowIdx) => {
+        const minLuy = row.split('').indexOf('#');
+        const maxLuy = row.split('').lastIndexOf('#') + 1;
+        
+        if (minLuy !== -1) {
+            const originLux = answer[0];
+            const originLuy = answer[1];
+            answer[0] = Math.min(originLux, rowIdx);
+            answer[1] = Math.min(originLuy, minLuy);
+            
+            if (minLuy !== maxLuy) {
+                const originRdx = answer[2];
+                const originRdy = answer[3];
+                answer[2] = Math.max(originRdx, rowIdx + 1);
+                answer[3] = Math.max(originRdy, maxLuy);
+            }
+        }
+        
+    })
+    
+    
+    return answer;
+}
