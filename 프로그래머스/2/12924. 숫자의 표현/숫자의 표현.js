489e064215ab872affd501161266dcdc1f50eb40
jinoc <132889294+jinoc-git@users.noreply.github.com>
2024-12-17 20:00:12 +0900

    [level 2] Title: 숫자의 표현, Time: 0.34 ms, Memory: 33 MB -BaekjoonHub

--- 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.js
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.js
@@ -2,10 +2,10 @@ function solution(n) {
     let answer = 0; 
     
     for (let i = 1; i <= n; i++) {
-        const isAliquot = n % i === 0; // 약수
+        const isSubmultiple = n % i === 0; // 약수
         const isOdd = i % 2 === 1; // 홀수
         
-        if (isAliquot && isOdd) answer++;
+        if (isSubmultiple && isOdd) answer++;
     }
 
     return answer;
