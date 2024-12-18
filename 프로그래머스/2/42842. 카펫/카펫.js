2ef104a23322ba608d9619760159c49e9e64492c
jinoc <132889294+jinoc-git@users.noreply.github.com>
2024-12-19 00:34:06 +0900

    [level 2] Title: 카펫, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.js
+++ 프로그래머스/2/42842. 카펫/카펫.js
@@ -2,6 +2,7 @@ function solution(brown, yellow) {
     const a = -2;
     const b = brown + 4;
     const c = -2 * brown - 2 * yellow;
+    
     const formular = b * b - 4 * a * c;
     const x = (-b - Math.sqrt(formular))  / (2 * a);
     const y = (-b + Math.sqrt(formular))  / (2 * a);
