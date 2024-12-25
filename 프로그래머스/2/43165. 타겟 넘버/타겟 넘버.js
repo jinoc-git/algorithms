3a47bfe72c8bee188f9ff7d8442692210225b3ad
jinoc <132889294+jinoc-git@users.noreply.github.com>
2024-12-25 16:05:22 +0900

    [level 2] Title: 타겟 넘버, Time: 15.10 ms, Memory: 35.5 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.js
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.js
@@ -1,17 +1,17 @@
 function solution(numbers, target) {
     let answer = 0;
     
-    const dfs = (index, sum) => {
+    const dfsFunc = (index, sum) => {
         if (index === numbers.length) {
             if (sum === target) answer++;
             return;
         }
         
-        dfs(index + 1, sum + numbers[index]);
-        dfs(index + 1, sum - numbers[index]);
+        dfsFunc(index + 1, sum + numbers[index]);
+        dfsFunc(index + 1, sum - numbers[index]);
     }
     
-    dfs(0, 0);
+    dfsFunc(0, 0);
     
     return answer;
 }
