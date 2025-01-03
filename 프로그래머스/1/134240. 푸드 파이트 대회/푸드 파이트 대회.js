228be040abe16f6b67ab64e58a313fe374d1d43b
jinoc <132889294+jinoc-git@users.noreply.github.com>
2025-01-04 01:39:43 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 0.63 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.js
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.js
@@ -1,17 +1,15 @@
 function solution(food) {
     let answer = '';
     let player1 = '';
-    let player2 = '';
     
     for (let i = 1; i < food.length; i++) {
         const playerFoodNum = Math.floor(food[i] / 2);
         if (playerFoodNum === 0) continue;
         
-        for (let j = 1; j <= playerFoodNum; j++) {
-            player1 += i;
-            player2 = i + player2;
-        }
+        for (let j = 1; j <= playerFoodNum; j++) player1 += i;
     }
+    
+    const player2 = player1.split('').reverse().join('');
     
     answer = player1 + '0' + player2;
     
