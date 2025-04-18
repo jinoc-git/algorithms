faadd7daad705effffe9938743f9a8429c965dc9
jinoc <132889294+jinoc-git@users.noreply.github.com>
2025-04-18 21:28:18 +0900

    [level 1] Title: 택배 상자 꺼내기, Time: 0.29 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/389478. 택배 상자 꺼내기/택배 상자 꺼내기.js
@@ -0,0 +1,35 @@
+function solution(n, w, num) {
+    const map = [];
+    let answer = 0;
+    let count = 1;
+    let x; 
+    let y;
+
+    for (let i = 0; i < Math.ceil(n / w); ++i) {
+        const row = [];
+        for (let j = 0; j < w; ++j) {
+            if (count > n) {
+                row.push(null);
+                continue;
+            }
+
+            if (count === num) {
+                y = i;
+                x = Math.ceil(count / w) % 2 === 0 ? w - j - 1 : j;
+            }
+
+            row.push(count);
+            ++count;
+        }
+
+        if (i % 2 === 1) row.reverse();
+        map.push(row);
+    }
+
+    while (y < map.length && map[y][x] !== null) {
+        ++answer;
+        ++y;
+    }
+
+    return answer;
+}
