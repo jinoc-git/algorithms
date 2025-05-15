40ac8b134cd63b3a9bea3d86c8b544d8d29cb281
노진철 <132889294+jinoc-git@users.noreply.github.com>
2025-05-15 21:03:11 +0900

    [level 1] Title: 유연근무제, Time: 5.09 ms, Memory: 33.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/388351. 유연근무제/유연근무제.js
@@ -0,0 +1,41 @@
+function solution(schedules, timelogs, startday) {
+    let answer = 0;
+    
+    for (let i = 0; i < schedules.length; i++) {
+        let fullSafe = true;
+        const targetTime = schedules[i];
+        const arrivalTimes = timelogs[i];
+        const safeTime = getSafeTime(targetTime);
+
+        for (let j = 0; j < arrivalTimes.length; j++) {
+            const isWeekend = (startday + j) % 7 === 0 || (startday + j) % 7 === 6;
+            if (isWeekend) continue;
+            
+            const isLate = safeTime < arrivalTimes[j];
+            if (isLate) {
+                fullSafe = false;
+                break;
+            }
+        }
+        
+        if (fullSafe) answer++;
+    }
+    
+    return answer;
+}
+
+const getSafeTime = (targetTime) => {
+    let result = 0;
+
+    const hour = Math.floor(targetTime / 100);
+    const minute = targetTime % 100;
+    
+    const hopeMinute = minute + 10;
+    if (hopeMinute >= 60) {
+        result = (hour + 1) * 100 + (hopeMinute - 60);
+    } else {
+        result = hour * 100 + hopeMinute;
+    }
+    
+    return result;
+}
