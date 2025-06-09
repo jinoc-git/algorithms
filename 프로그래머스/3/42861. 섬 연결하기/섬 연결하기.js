79772057563f75be32641a9954bc20d8a859059d
노진철 <132889294+jinoc-git@users.noreply.github.com>
2025-06-10 00:35:12 +0900

    [level 3] Title: 섬 연결하기, Time: 0.34 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.js
@@ -0,0 +1,36 @@
+function solution(n, costs) {
+    let answer = 0;
+    const ascendingCosts = costs.sort((a, b) => a[2] - b[2]);
+    const islandGroup = Array.from({length: n}, (_, i) => i);
+    
+    const getParentIsland = (islandId) => {
+        if (islandGroup[islandId] === islandId) return islandId;
+        
+        islandGroup[islandId] = getParentIsland(islandGroup[islandId]);
+        return islandGroup[islandId];
+    }
+    
+    const joinGroup = (islandA, islandB) => {
+        const parentA = getParentIsland(islandA);
+        const parentB = getParentIsland(islandB);
+        
+        if (parentA !== parentB) {
+            islandGroup[parentB] = parentA;
+            return true;
+        }
+        return false;
+    }
+    
+    let connectedBridge = 0;
+    
+    for (let [islandA, islandB, cost] of costs) {
+        if (joinGroup(islandA, islandB)) {
+            answer += cost;
+            connectedBridge++;
+            
+            if (connectedBridge === n - 1) break;
+        }
+    }
+    
+    return answer;
+}
