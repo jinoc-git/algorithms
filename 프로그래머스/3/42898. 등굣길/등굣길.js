5fbbe55f22d37a3e4e0b5351cabfeeaa42c2e396
노진철 <132889294+jinoc-git@users.noreply.github.com>
2025-06-16 18:29:35 +0900

    [level 3] Title: 등굣길, Time: 4.60 ms, Memory: 37.1 MB -BaekjoonHub

--- 프로그래머스/3/42898. 등굣길/등굣길.js
+++ 프로그래머스/3/42898. 등굣길/등굣길.js
@@ -4,15 +4,14 @@ function solution(m, n, puddles) {
     const raw = 1_000_000_007;
     // const ways = new Array(n + 1).fill(new Array(m + 1).fill(0));
     const ways = Array.from({ length: n + 1 }, () => Array(m + 1).fill(0));
-    
     const traps = new Set();
-    puddles.forEach(([x, y]) => traps.add(`${x},${y}`));
+    puddles.forEach(([x, y]) => traps.add(`${y},${x}`));
     
     ways[1][1] = 1;
 
     for (let y = 1; y <= n; y++) {
         for (let x = 1; x <= m; x++) {
-            const isTrap = traps.has(`${x},${y}`);
+            const isTrap = traps.has(`${y},${x}`);
             if (isTrap) {
                 ways[y][x] = 0;
                 continue;
@@ -21,8 +20,8 @@ function solution(m, n, puddles) {
             const isHome = x === 1 && y === 1;
             if (isHome) continue;
             
-            const topToDownWayCount = ways.at(y - 1).at(x);
-            const leftToRightWayCount = ways.at(y).at(x - 1);
+            const topToDownWayCount = ways[y - 1][x];
+            const leftToRightWayCount = ways[y][x - 1];
             ways[y][x] = (topToDownWayCount + leftToRightWayCount) % raw;
         }
     }
