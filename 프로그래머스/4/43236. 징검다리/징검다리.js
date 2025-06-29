f45afc6efc3d12db1f9f61ada5298fa6c4b0eab4
노진철 <132889294+jinoc-git@users.noreply.github.com>
2025-06-29 22:34:51 +0900

    [level 4] Title: 징검다리, Time: 149.35 ms, Memory: 40.7 MB -BaekjoonHub

---
+++ 프로그래머스/4/43236. 징검다리/징검다리.js
@@ -0,0 +1,31 @@
+function solution(distance, rocks, n) {
+    rocks.sort((a, b) => a - b);
+    const ascendingRocks = [0, ...rocks, distance];
+    
+    let answer = 0;
+    let low = 1;
+    let high = distance;
+    
+    while (low <= high) {
+        let mid = Math.floor((low + high) / 2);
+        let removeRockCount = 0;
+        let position = 0;
+        
+        for (let i = 1; i < ascendingRocks.length; i++) {
+            const curRock = ascendingRocks.at(i);
+            const gap = curRock - position;
+            
+            if (gap < mid) removeRockCount++;
+            else position = curRock;
+        }
+        if (removeRockCount <= n) {
+            answer = mid;
+            low = mid + 1;
+        } else {
+            high = mid - 1;
+        }
+    }
+    
+    
+    return answer;
+}
