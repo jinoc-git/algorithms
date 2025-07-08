546e4fbd479940cb99dcfb05730616b70af65301
노진철 <132889294+jinoc-git@users.noreply.github.com>
2025-07-08 20:32:47 +0900

    [level 3] Title: 가장 먼 노드, Time: 41.04 ms, Memory: 56.2 MB -BaekjoonHub

---
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.js
@@ -0,0 +1,38 @@
+function solution(n, edge) {
+    const graph = Array.from({ length: n + 1 }, () => []);
+    for (const [from, to] of edge) {
+        graph[from].push(to);
+        graph[to].push(from);
+    }
+    
+    const history = Array.from({ length: n + 1 }, () => ({ distance: -1, visited: false }));
+    
+    const queue = [[1, 0]];
+    history[1].distance = 0;
+    history[1].visited = true;
+    
+    let front = 0;
+    let maxDistance = 0;
+    
+    while (front < queue.length) {
+        const [curNode, curDistance] = queue[front];
+        front++;
+        
+        if (curNode > maxDistance) maxDistance = curDistance;
+        
+        for (const neighbor of graph[curNode]) {
+            if (history[neighbor].visited === false) {
+                history[neighbor].visited = true;
+                history[neighbor].distance = curDistance + 1;
+                queue.push([neighbor, curDistance + 1]);
+            }
+        }
+    }
+    
+    let answer = 0;
+    for (let i = 1; i <= n; i++) {
+        if (history[i].distance === maxDistance) answer++;
+    }
+    
+    return answer;
+}
