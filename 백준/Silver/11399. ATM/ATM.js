279fb56b516a19ea516a0ae895b13b058fd7f915
노진철 <132889294+jinoc-git@users.noreply.github.com>
2025-08-28 00:31:20 +0900

    [Silver IV] Title: ATM, Time: 156 ms, Memory: 9892 KB -BaekjoonHub

--- 백준/Silver/11399. ATM/ATM.js
+++ 백준/Silver/11399. ATM/ATM.js
@@ -11,15 +11,13 @@ const input = fs
  * @param {number[]} times 각 사람들이 인출할 때 걸리는 시간들
  */
 const solution = (N, times) => {
-  const ascendingTimes = times
-    .map((v, idx) => [v, idx])
-    .sort((a, b) => a[0] - b[0]);
+  const ascendingTimes = times.sort((a, b) => a - b);
 
   let answer = 0;
   for (let i = 0; i < N; i++) {
-    answer += ascendingTimes[i][0];
+    answer += ascendingTimes[i];
     for (let j = 0; j < i; j++) {
-      answer += ascendingTimes[j][0];
+      answer += ascendingTimes[j];
     }
   }
 
