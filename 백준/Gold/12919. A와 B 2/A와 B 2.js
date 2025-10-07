f19830d36fa105ffc00f264b3daa36ebf4d5dd38
노진철 <132889294+jinoc-git@users.noreply.github.com>
2025-10-07 15:02:09 +0900

    [Gold V] Title: A와 B 2, Time: 92 ms, Memory: 9356 KB -BaekjoonHub

---
+++ 백준/Gold/12919. A와 B 2/A와 B 2.js
@@ -0,0 +1,44 @@
+const fs = require('fs');
+const input = fs
+  .readFileSync('/dev/stdin')
+  .toString()
+  .trim()
+  .split('\n');
+
+/**
+ * T -> S 역순으로 변환하기, 조건 2개 모두 적용 가능한 경우가 있기에 DFS 사용
+ * @param {string} S 시작 문자열
+ * @param {string} T 목표하는 문자열
+ */
+const solution = (S, T) => {
+  let answer = 0;
+
+  /**
+   *
+   * @param {string} curStr 현재 문자열
+   * @returns
+   */
+  const dfs = (curStr) => {
+    if (answer === 1) return;
+    if (curStr.length === S.length) {
+      if (curStr === S) answer = 1;
+      return;
+    }
+
+    if (curStr.endsWith('A')) {
+      dfs(curStr.slice(0, -1));
+    }
+
+    if (curStr.startsWith('B')) {
+      const nextStr = curStr.slice(1).split('').reverse().join('');
+      dfs(nextStr);
+    }
+  };
+
+  dfs(T);
+
+  return answer;
+};
+
+const [S, T] = input.map((v) => v.trim());
+console.log(solution(S, T));
