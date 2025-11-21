fd4977fb8d488c4c064439fdb41fec558913cab3
노진철 <132889294+jinoc-git@users.noreply.github.com>
2025-11-21 16:46:07 +0900

    [Gold IV] Title: 전화번호 목록, Time: 308 ms, Memory: 34204 KB -BaekjoonHub

---
+++ 백준/Gold/5052. 전화번호 목록/전화번호 목록.js
@@ -0,0 +1,44 @@
+/**
+ *
+ * @param {number} t 테스트 개수
+ * @param {string[][]} tCases 테스트 케이스들
+ */
+const solution = (t, tCases) => {
+  const results = [];
+
+  tCases.forEach((testCase) => {
+    testCase.sort();
+
+    let isGood = true;
+    for (let i = 0; i < testCase.length - 1; i++) {
+      const curString = testCase[i];
+      const nextString = testCase[i + 1];
+      if (nextString.startsWith(curString)) {
+        isGood = false;
+        results.push('NO');
+        break;
+      }
+    }
+
+    if (isGood) results.push('YES');
+  });
+
+  return results.join('\n');
+};
+
+const fs = require('fs');
+const path = require('path');
+const filePath =
+  process.platform === 'linux'
+    ? '/dev/stdin'
+    : path.join(__dirname, 'input2.txt');
+const input = fs.readFileSync(filePath).toString('utf8').trim().split('\n');
+
+const t = parseInt(input.shift());
+const tCases = [];
+for (let i = 1; i <= t; i++) {
+  const tCaseCount = parseInt(input.shift());
+  const tCase = input.splice(0, tCaseCount).map((v) => v.trim());
+  tCases.push(tCase);
+}
+console.log(solution(t, tCases));
