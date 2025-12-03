e9672b2af6892b803d243dbc6433f9cefbad24dd
노진철 <132889294+jinoc-git@users.noreply.github.com>
2025-12-03 22:34:23 +0900

    [Bronze II] Title: 피보나치 수, Time: 88 ms, Memory: 9600 KB -BaekjoonHub

---
+++ 백준/Bronze/2747. 피보나치 수/피보나치 수.js
@@ -0,0 +1,19 @@
+function solution(input) {
+  const n = parseInt(input);
+  const dp = new Array(n + 1);
+
+  dp[0] = 0;
+  dp[1] = 1;
+
+  for (let i = 2; i <= n; i++) {
+    dp[i] = dp[i - 1] + dp[i - 2];
+  }
+
+  return dp[n];
+}
+
+const fs = require('fs');
+const path = require('path');
+const filePath = process.platform === 'linux' ? '/dev/stdin' : path.join(__dirname, 'input3.txt');
+const input = fs.readFileSync(filePath).toString();
+console.log(solution(input));
